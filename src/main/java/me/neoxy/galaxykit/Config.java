0bf5309e06bb56930a87dbee3b6c643839e81b14
Neoxy <1007050740@qq.com>
2022-07-31 12:44:06 +0800

    Example language files

--- src/main/java/me/neoxy/galaxykit/Config.java
+++ src/main/java/me/neoxy/galaxykit/Config.java
@@ -2,10 +2,21 @@ package me.neoxy.galaxykit;
 
 import org.yaml.snakeyaml.Yaml;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 
 public class Config {
-    File file = new File("./plugins/galaxykit/config.yml");
+    public void loadConfig(){
+        try{
+            BufferedReader i= new BufferedReader(new FileReader("./plugins/galaxykit/config.yml"));
+            //Array
+            String content=i.readLine();
+        }
+        catch(Exception e){
 
+        }
+    }
 }
